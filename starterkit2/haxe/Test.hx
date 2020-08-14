8996e150a49fd1edc59a470e609b54b6a244a967
nn4343 <gea01441@nifty.com>
2020-08-14 11:04:50 +0900

    Haxe追加

---
+++ starterkit2/haxe/Test.hx
@@ -0,0 +1,10 @@
+import haxe.xml.Access;
+
+class Test {
+    static public function main() {
+        trace("#START");
+        var sm = new TestControl();
+        sm.Run();
+        trace("#END");
+    }
+}
