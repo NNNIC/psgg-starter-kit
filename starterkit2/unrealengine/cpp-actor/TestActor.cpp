f1fe63d701ec99598f220cbd47e37898acf9ba30
programanic <statego@programanic.com>
2024-03-20 19:44:23 +0900

    For unreal engine 5 Actor　

---
+++ starterkit2/unrealengine/cpp-actor/TestActor.cpp
@@ -0,0 +1,163 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestActor.h"
+
+//  Embed codes                              [STATE-GO OUTPUT START] indent(0) $/^E_/$
+//             psggConverterLib.dll converted from psgg-file:TestActor.psgg // *DoNotEdit*
+                                                                            // *DoNotEdit*
+                                                                            // *DoNotEdit*
+                                                                            // *DoNotEdit*
+//                                           [STATE-GO OUTPUT END]
+
+// Sets default values
+ATestActor::ATestActor() :
+    //  construction codes                       [STATE-GO OUTPUT START] indent(8) $/^S_/->#constructlist$
+        //             psggConverterLib.dll converted from psgg-file:TestActor.psgg                                 // *DoNotEdit*
+                                                                            // *DoNotEdit*
+        m_i(0),                                                             // *DoNotEdit*
+                                                                            // *DoNotEdit*
+//                                           [STATE-GO OUTPUT END]
+m_curstate(nullptr), m_nextstate(nullptr), m_bNoWait(false), m_callstack_level(0)
+{
+    for(int i = 0; i < CALL_STACK_MAX; i++) m_callstack[i] = nullptr;
+
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts or when spawned
+void ATestActor::BeginPlay()
+{
+	Super::BeginPlay();
+    Start();
+	
+}
+
+// Called every frame
+void ATestActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+    if (!IsEnd()) {
+        Update();
+    }
+}
+
+void ATestActor::_update()
+{
+    while (true) {
+        bool bFirst = false;
+        m_bNoWait = false;
+        if (m_nextstate != nullptr) {
+            m_curstate = m_nextstate;
+            m_nextstate = nullptr;
+            bFirst = true;
+        }
+        if (m_curstate != nullptr) {
+            (this->*m_curstate)(bFirst);
+        }
+        if (!m_bNoWait) break;
+    }
+}
+
+void ATestActor::Start()
+{
+    Goto(&ATestActor::S_START);
+}
+bool ATestActor::IsEnd()
+{
+    return CheckState(&ATestActor::S_END);
+}
+void ATestActor::Update()
+{
+    _update();
+}
+
+//  states                                   [STATE-GO OUTPUT START] indent(0) $/^S_/$
+//             psggConverterLib.dll converted from psgg-file:TestActor.psgg // *DoNotEdit*
+                                                                            // *DoNotEdit*
+/*                                                                          // *DoNotEdit*
+    S_0000                                                                  // *DoNotEdit*
+*/                                                                          // *DoNotEdit*
+void ATestActor::S_0000(bool bFirst)                                        // *DoNotEdit*
+{                                                                           // *DoNotEdit*
+    if (bFirst)                                                             // *DoNotEdit*
+    {                                                                       // *DoNotEdit*
+        LOG("HELLO STATEGO");                                               // *DoNotEdit*
+    }                                                                       // *DoNotEdit*
+    if (!HasNextState())                                                    // *DoNotEdit*
+    {                                                                       // *DoNotEdit*
+        Goto(&ATestActor::S_LOP000);                                        // *DoNotEdit*
+    }                                                                       // *DoNotEdit*
+}                                                                           // *DoNotEdit*
+/*                                                                          // *DoNotEdit*
+    S_0001                                                                  // *DoNotEdit*
+*/                                                                          // *DoNotEdit*
+void ATestActor::S_0001(bool bFirst)                                        // *DoNotEdit*
+{                                                                           // *DoNotEdit*
+    if (bFirst)                                                             // *DoNotEdit*
+    {                                                                       // *DoNotEdit*
+        LOG("HI!!!");                                                       // *DoNotEdit*
+    }                                                                       // *DoNotEdit*
+    if (!HasNextState())                                                    // *DoNotEdit*
+    {                                                                       // *DoNotEdit*
+        Goto(&ATestActor::S_RET000);                                        // *DoNotEdit*
+    }                                                                       // *DoNotEdit*
+}                                                                           // *DoNotEdit*
+/*                                                                          // *DoNotEdit*
+    S_END                                                                   // *DoNotEdit*
+*/                                                                          // *DoNotEdit*
+void ATestActor::S_END(bool bFirst)                                         // *DoNotEdit*
+{                                                                           // *DoNotEdit*
+}                                                                           // *DoNotEdit*
+/*                                                                          // *DoNotEdit*
+    S_LOP000                                                                // *DoNotEdit*
+*/                                                                          // *DoNotEdit*
+void ATestActor::S_LOP000(bool bFirst)                                      // *DoNotEdit*
+{                                                                           // *DoNotEdit*
+    m_i = 0;                                                                // *DoNotEdit*
+    Goto(&ATestActor::S_LOP000_LoopCheckAndGosub____);                      // *DoNotEdit*
+    NoWait();                                                               // *DoNotEdit*
+}                                                                           // *DoNotEdit*
+void ATestActor::S_LOP000_LoopCheckAndGosub____(bool bFirst)                // *DoNotEdit*
+{                                                                           // *DoNotEdit*
+    if (m_i < 10) GoSubState(&ATestActor::S_SBS000,&ATestActor::S_LOP000_LoopNext____);                             // *DoNotEdit*
+    else               Goto(&ATestActor::S_END);                            // *DoNotEdit*
+    NoWait();                                                               // *DoNotEdit*
+}                                                                           // *DoNotEdit*
+void ATestActor::S_LOP000_LoopNext____(bool bFirst)                         // *DoNotEdit*
+{                                                                           // *DoNotEdit*
+    m_i++;                                                                  // *DoNotEdit*
+    Goto(&ATestActor::S_LOP000_LoopCheckAndGosub____);                      // *DoNotEdit*
+    NoWait();                                                               // *DoNotEdit*
+}                                                                           // *DoNotEdit*
+/*                                                                          // *DoNotEdit*
+    S_RET000                                                                // *DoNotEdit*
+*/                                                                          // *DoNotEdit*
+void ATestActor::S_RET000(bool bFirst)                                      // *DoNotEdit*
+{                                                                           // *DoNotEdit*
+    ReturnState();                                                          // *DoNotEdit*
+    NoWait();                                                               // *DoNotEdit*
+}                                                                           // *DoNotEdit*
+/*                                                                          // *DoNotEdit*
+    S_SBS000                                                                // *DoNotEdit*
+*/                                                                          // *DoNotEdit*
+void ATestActor::S_SBS000(bool bFirst)                                      // *DoNotEdit*
+{                                                                           // *DoNotEdit*
+    Goto(&ATestActor::S_0001);                                              // *DoNotEdit*
+    NoWait();                                                               // *DoNotEdit*
+}                                                                           // *DoNotEdit*
+/*                                                                          // *DoNotEdit*
+    S_START                                                                 // *DoNotEdit*
+*/                                                                          // *DoNotEdit*
+void ATestActor::S_START(bool bFirst)                                       // *DoNotEdit*
+{                                                                           // *DoNotEdit*
+    Goto(&ATestActor::S_0000);                                              // *DoNotEdit*
+    NoWait();                                                               // *DoNotEdit*
+}                                                                           // *DoNotEdit*
+                                                                            // *DoNotEdit*
+                                                                            // *DoNotEdit*
+//                                           [STATE-GO OUTPUT END]
+// write your code here!
---
+++ starterkit2/unrealengine/cpp-actor/TestActor.h
@@ -0,0 +1,99 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TestActor.generated.h"
+
+#define LOG(STR) UE_LOG(LogTemp, Log, TEXT(STR))
+#define ERROR(STR) UE_LOG(LogTemp, Error, TEXT(STR))
+
+UCLASS()
+class UPROJECT_API ATestActor : public AActor
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this actor's properties
+	ATestActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+private:
+	// STATEGO FRAMEWORK
+	typedef void (ATestActor::* FUNCPTR)(bool);
+
+	FUNCPTR m_curstate;
+	FUNCPTR m_nextstate;
+	bool   m_bNoWait;
+
+	static const int CALL_STACK_MAX = 10;
+	FUNCPTR m_callstack[CALL_STACK_MAX];
+	int m_callstack_level;
+
+	void _update();
+    inline void Goto(FUNCPTR st) { m_nextstate = st; }
+    inline bool CheckState(FUNCPTR st) { return m_curstate == st; }
+    inline bool HasNextState() { return m_nextstate != nullptr; }
+    inline void NoWait() { m_bNoWait = true; }
+    inline void GoSubState(FUNCPTR sb, FUNCPTR nt)
+    {
+        if (m_callstack_level >= CALL_STACK_MAX - 1)
+        {
+            LOG("CALL STACK OVER FLOW\n");
+            //exit(1);
+        }
+        m_callstack[m_callstack_level++] = nt;
+        Goto(sb);
+    }
+    inline void ReturnState()
+    {
+        if (m_callstack_level <= 0)
+        {
+            LOG("CALL STACK UNDER FLOW\n");
+        }
+        else {
+            m_callstack_level--;
+            FUNCPTR st = this->m_callstack[m_callstack_level];
+            Goto(st);
+        }
+    }
+public:
+	void Start();
+	bool IsEnd();
+	void Update();
+
+//  Member codes                              [STATE-GO OUTPUT START] indent(4) $/^S_/->#memberlist$
+    //             psggConverterLib.dll converted from psgg-file:TestActor.psgg                                     // *DoNotEdit*
+                                                                            // *DoNotEdit*
+    int m_i;                                                                // *DoNotEdit*
+                                                                            // *DoNotEdit*
+//                                           [STATE-GO OUTPUT END]
+
+
+//  API declaration                          [STATE-GO OUTPUT START] indent(4) $/^S_/->#declare$
+    //             psggConverterLib.dll converted from psgg-file:TestActor.psgg                                     // *DoNotEdit*
+                                                                            // *DoNotEdit*
+    void S_0000(bool bFirst);                                               // *DoNotEdit*
+    void S_0001(bool bFirst);                                               // *DoNotEdit*
+    void S_END(bool bFirst);                                                // *DoNotEdit*
+    void S_LOP000(bool bFirst);                                             // *DoNotEdit*
+    void S_LOP000_LoopCheckAndGosub____(bool bFirst);                       // *DoNotEdit*
+    void S_LOP000_LoopNext____(bool bFirst);                                // *DoNotEdit*
+    void S_RET000(bool bFirst);                                             // *DoNotEdit*
+    void S_SBS000(bool bFirst);                                             // *DoNotEdit*
+    void S_START(bool bFirst);                                              // *DoNotEdit*
+                                                                            // *DoNotEdit*
+                                                                            // *DoNotEdit*
+//                                           [STATE-GO OUTPUT END]
+
+
+};
